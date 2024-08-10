5df5bf3a05439b5362a5f7c9881c3e7e3bfda59b
root <root@trouvers2123.cafe24.com>
2024-08-10 19:56:25 +0900

    save

--- server/routes/haksik.py
+++ server/routes/haksik.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
+import datetime
 
 haksik_router = APIRouter(
   prefix="/haksik"
 )
 
 @haksik_router.get('')
-async def get_haksik():
+async def get_haksik(request: Request):
   haksikData = {
     'menu' : {
       # 앞에서부터 중식, A, B, C, D, E, F
@@ -14,7 +15,12 @@ async def get_haksik():
       'TUE' : ['참치마요', '비지찌개', '소고기무국', '고추참치마요덮밥', '후랑크갈릭볶음밥', '낙불새덮밥 *(낙지, 불고기, 새우)', '닭개장'],
       'WED' : ['참치마요', '돼지고기김치찌개', '우삼겹된장찌개', '', '', '소고기유부볶음밥', ''],
       'THU' : ['', '짬뽕', '해물우동', '돼지순두부찌개', '일식카레*가라아게', '갈비찜', ''],
-      'FRI': ['', '차슈덮밥', '오꼬노미야끼+미니소바', '사케동', '명란마요덮밥', '', ''],
+      'FRI': ['', '진우덮밥', '오꼬노미야끼+미니소바', '사케동', '명란마요덮밥', '', ''],
     }
   }
+  
+  clinet_ip = request.client.host
+  current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  
+  print(f"요청 : {clinet_ip} at {current_time}")
   return JSONResponse(status_code=200, content={'success' : True, 'body' : haksikData})
