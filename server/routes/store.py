95c9ba55179d8fff1d0e62a9dec6114fc4c8b1d9
myeongcode <woomg427@yonsei.ac.kr>
2024-09-19 01:52:34 +0900

    Fix: refactoring v1

--- server/routes/store.py
+++ server/routes/store.py
@@ -12,20 +12,10 @@ store_router = APIRouter(
   prefix="/store"
 )
 
-
 @store_router.get('', response_model=list[StoreListSchema], summary="전체 매장 조회")
 async def get_store_list(db: Session = Depends(get_db)):
   # 연세대 미래
   store_list = db.query(Store).filter(Store.school_id == 1).all()
-    
-  return store_list
-
-
-# 음식점 매장 조회
-@store_router.get('/food', response_model=list[StoreListSchema], summary="음식점 매장 조회")
-async def get_food_store_list(db: Session = Depends(get_db)):
-  # 연세대 미래
-  store_list = db.query(Store).filter(Store.school_id == 1, Store.category_id == 1).all()
     
   return store_list
 
